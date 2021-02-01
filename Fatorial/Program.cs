59863e5f830508b6e31594974a8dd663672db54d
dpa0702 <56276309+dpa0702@users.noreply.github.com>
2021-02-01 16:58:33 -0300

    Carga Inicial
    CAga inicial

---
+++ Fatorial/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Fatorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fatorial(0);
+            Fatorial(1);
+            Fatorial(2);
+            Fatorial(3);
+            Fatorial(4);
+            Fatorial(5);
+            Fatorial(6);
+            Fatorial(7);
+            Fatorial(8);
+            Fatorial(9);
+            Fatorial(10);
+            Fatorial(11);
+        }
+
+        public static void Fatorial(int num)
+        {
+            var total = 1;
+            var texto = "";
+
+            for (var a = num; a > 0; a--)
+            {
+                total = total * a;
+                texto = texto + a + "x";
+            }
+
+            if(num != 0)
+            {
+                Console.WriteLine(num + "! = " + texto.Substring(0, (texto.Length - 1)) + " = " + total);
+            }
+            else
+            {
+                Console.WriteLine(num + "! = " + total);
+            }
+        }
+    }
+}
